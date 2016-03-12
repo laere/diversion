bf048c24e8551a1de864fe764e58ed3f59277459
Zack Bostian <bostianz@gmail.com>
2016-03-12 09:18:47 -0500

    matched header color to hover colorof dashboard icons

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -10,18 +10,22 @@ export default class Dashboard extends Component {
         videos
         userss*/}
         <DashboardLink
+          className="dashboardLink streamsLink"
           link="streams"
           icon="fa fa-desktop fa-2x"
         />
         <DashboardLink
+          className="dashboardLink gamesLink"
           link="games"
           icon="fa fa-gamepad fa-2x"
         />
         <DashboardLink
+          className="dashboardLink videosLink"
           link="videos"
           icon="fa fa-video-camera fa-2x"
         />
         <DashboardLink
+          className="dashboardLink usersLink"
           link="users"
           icon="fa fa-users fa-2x"
         />
