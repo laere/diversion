f31e66fefdb6219b118d1bd89dfd60fef3f5aa3f
Zack Bostian <bostianz@gmail.com>
2016-03-12 16:28:58 -0500

    added twitch auth, but need to host site for it to work

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,7 +5,7 @@ const Searchbar = ( { input, onChange } ) => {
   return (
     <div>
       <div className="twitchButton">
-        <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" class="twitch-connect" href="#" />
+        <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" className="twitch-connect" href="#" />
       </div>
       <div className="searchbar">
         <input
