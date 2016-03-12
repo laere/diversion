731f3db12ac7d71d1985fda2996e08b92eab1e32
Zack Bostian <bostianz@gmail.com>
2016-03-12 11:41:58 -0500

    added active styling to dashboard icons

--- src/components/DashboardLink.js
+++ src/components/DashboardLink.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const DashboardLink = (props) => {
 
     return (
-      <Link to={props.link}>
+      <Link to={props.link} style={{color: '#73797F' }} activeStyle={{backgroundColor: props.bgColor, color: props.fontColor}} >
         <div className={props.className}>
           <i className={props.icon}></i>
           <div className="dashboardName">{props.link}</div>
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,27 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import DashboardLink from '../components/DashboardLink';
-import { selectDashboardIcon } from '../actions/actions';
 
-class Dashboard extends Component {
-
-  constructor(props) {
-    super(props);
-    this.handleSelectIconClick = this.handleSelectIconClick.bind(this);
-  }
-
-  handleSelectIconClick(e) {
-    e.preventDefault();
-    console.log('click click');
-    // const { selectDashboardIcon } = this.props;
-    // selectDashboardIcon();
-  }
-
+export default class Dashboard extends Component {
   render() {
-
-    // const { dashboardIconSelected } = this.props;
-    // const selected = dashboardIconSelected ? '#000' : '#fff';
-
     return (
       <div className="dashboard">
 
@@ -29,35 +10,35 @@ class Dashboard extends Component {
           className="dashboardLink streamsLink"
           link="streams"
           icon="fa fa-desktop fa-2x"
+          bgColor={'#09BC8A'}
+          fontColor={'#fff'}
         />
 
         <DashboardLink
           className="dashboardLink gamesLink"
           link="games"
           icon="fa fa-gamepad fa-2x"
+          bgColor={'#E84C4C'}
+          fontColor={'#fff'}
         />
 
         <DashboardLink
           className="dashboardLink videosLink"
           link="videos"
           icon="fa fa-video-camera fa-2x"
+          bgColor={'#6870B8'}
+          fontColor={'#fff'}
         />
 
         <DashboardLink
           className="dashboardLink usersLink"
           link="users"
           icon="fa fa-users fa-2x"
+          bgColor={'#774C60'}
+          fontColor={'#fff'}
         />
 
       </div>
     );
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    dashboardIconSelected: state.dashboardIconSelected
-  };
-};
-
-export default connect(mapStateToProps, { selectDashboardIcon } )(Dashboard);
